2dc93335ccf4ca5dded1c0a8830e8c49807ee5ba
surekha25 <surekhasuri25@gmail.com>
2026-02-24 15:51:55 +0530

    terraform

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+module "catalogue"{
+    source = "../terraform-aws-instance"
+    # ami_id = "ami-09c813fb71547fc4f"
+    # instance_type = "t3.micro"
+    # sg_ids = ["sg-026125864f002d1cd"]
+
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    sg_ids = var.sg_ids
+    tags = {
+        Name = "Modules-Demo"
+    }
+}
+
+output "public_ip" {
+    value = module.catalogue.public_ip
+}
+
+output "private_ip" {
+    value = module.catalogue.private_ip
+}   
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "sg_ids" {
+    default = ["sg-026125864f002d1cd"]
+}
+
+variable "instance_type" {
+    # default = "t3.micro"
+}
